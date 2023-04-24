94ef3bed1a7fcd0a82dd2cedf8cd403d8994688a
Darksied95 <aliyuefe9501@gmail.com>
2023-04-24 18:31:40 +0100

    added basic html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,51 @@
 import "./App.css";
+import Cross from "./assets/icon-cross.svg";
 
-function App() {}
+function App() {
+  return (
+    <main>
+      <h1>TODO</h1>
+      <input type="text" placeholder="Create a new todo..." />
+      <div>
+        <ul>
+          <li>
+            <span />
+            <p>Lorem ipsum dolor, sit amet conse</p>
+            <img src={Cross} />
+          </li>
+          <li>
+            <span />
+            <p>Lorem ipsum dolor, sit amet conse</p>
+            <img src={Cross} />
+          </li>
+          <li>
+            <span />
+            <p>Lorem ipsum dolor, sit amet conse</p>
+            <img src={Cross} />
+          </li>
+          <li>
+            <span />
+            <p>Lorem ipsum dolor, sit amet conse</p>
+            <img src={Cross} />
+          </li>
+          <li>
+            <span />
+            <p>Lorem ipsum dolor, sit amet conse</p>
+            <img src={Cross} />
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>5 items left</p>
+        <div>
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+          <button>Clear completed</button>
+        </div>
+      </div>
+    </main>
+  );
+}
 
 export default App;
