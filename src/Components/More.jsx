1084265a458e8c529a052cc1cd0efd0f178b207e
Darksied95 <aliyuefe9501@gmail.com>
2023-04-25 22:58:55 +0100

    items remaining bug fixed

--- src/Components/More.jsx
+++ src/Components/More.jsx
@@ -1,9 +1,11 @@
 import SortButtons from "./SortButtons";
+import useGlobalHook from "./../Hooks/useGlobalHook";
 
 const More = () => {
+  const { todoLists } = useGlobalHook();
   return (
     <div className="bg-ex-blue-500 text-ex-grayBlue-300 font-bold flex justify-between py-5 px-3 mb-16">
-      <p className=" ">5 items left</p>
+      <p className=" ">{todoLists.length} items left</p>
       <SortButtons />
       <button>Clear completed</button>
     </div>
