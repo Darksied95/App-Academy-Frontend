477ba7a26bbb3b4e7d17fe9058e034493ac4f806
Darksied95 <aliyuefe9501@gmail.com>
2023-04-26 16:09:05 +0100

    switch img from relative to absolute paths

--- src/Pages/Todo.jsx
+++ src/Pages/Todo.jsx
@@ -7,7 +7,7 @@ const Todo = () => {
   if (!token) return (window.location = "/");
 
   return (
-    <main className="bg-mobile bg-no-repeat bg-contain   bg-ex-blue-300 min-h-screen  pt-1 px-6 text-sm text-ex-grayBlue-100 md:bg-desktop">
+    <main className="bg-[url('https://i.ibb.co/PrcYxMs/bg-mobile-dark.jpg')] md:bg-[url('https://i.ibb.co/K5nWphm/bg-desktop-dark.jpg')] bg-no-repeat bg-contain   bg-ex-blue-300 min-h-screen  pt-1 px-6 text-sm text-ex-grayBlue-100">
       <div className="max-w-[720px] mx-auto">
         <h1 className="text-2xl  font-bold tracking-[.5rem] mt-10 mb-8 text-white md:mt-16">
           TODO
